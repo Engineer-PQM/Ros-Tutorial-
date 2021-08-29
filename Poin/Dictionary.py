7752d6598f00aa2178dab5a9cbe811df00a18d8d
Minh Phạm Quang <82381342+Engineer-PQM@users.noreply.github.com>
2021-08-30 00:53:32 +0700

    Create Dictionary.py

---
+++ Poin/Dictionary.py
@@ -0,0 +1,27 @@
+Muti_Poin = {
+    "name": "Path from A to B",
+    'waypoints1': {
+                'position_x': 0.9,
+                'position_y': 0.5,
+                'orientation': 1.0
+            },
+    'waypoints2': {
+                'position_x': 0.6,
+                'position_y': 0.2,
+                'orientation': 1.0
+            },
+    'waypoints3': {
+                'position_x': 0.2,
+                'position_y': 0.9,
+                'orientation': 1.0
+            }             
+}
+#Position['positionX']
+#print(Muti_Poin.get('positionX'))
+for x in Muti_Poin.values():
+    print(x)
+
+def hello():
+    print("minhdeptrai")
+    y = Muti_Poin['waypoints1']['orientation']
+   # print(y)  
